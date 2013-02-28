a963ad907e0ef9fc082e4aa1edd48c23037852f2
ffacon <francois.facon@gmail.com>
2013-02-28 18:32:29 +0100

    5.3.4-SNAPSHOT

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-﻿# Tapestry 5 Portlet extension for Liferay - 5.3.3
+﻿# Tapestry 5 Portlet extension for Liferay - 5.3.4-SNAPSHOT
 
 ## List Of Contributors
 - François FACON as got5     
 - Emmanuel DEMEY as Gillespie59
 
 ## Changelog
-- 5.3.3 : First Release for Tapestry 5.3.6 and Tapestry5-portlet 5.3.3
+- 5.3.3 : First Release for Tapestry 5.3.6 & Tapestry5-portlet 5.3.3
 - 5.3.3-SNAPSHOT : Initial Commit
 
 ## License
--- tapestry-liferay/src/test/java/org/apache/tapestry5/portlet/services/AppModule.java
+++ tapestry-liferay/src/test/java/org/apache/tapestry5/portlet/services/AppModule.java
@@ -9,7 +9,7 @@ import org.apache.tapestry5.portlet.upload.services.PortletUploadModule;
  * This module is automatically included as part of the Tapestry IoC Registry, it's a good place to
  * configure and extend Tapestry, or to place your own service definitions.
  */
-@SubModule({PortletModule.class, PortletUploadModule.class})
+
 public class AppModule
 {
 
@@ -19,7 +19,7 @@ public class AppModule
 
         configuration.add(SymbolConstants.SUPPORTED_LOCALES, "en");
         configuration.add(SymbolConstants.PRODUCTION_MODE, "false");
-        configuration.add(SymbolConstants.APPLICATION_VERSION, "5.3.3");
+        configuration.add(SymbolConstants.APPLICATION_VERSION, "5.3.4-SNAPSHOT");
         configuration.add(SymbolConstants.APPLICATION_CATALOG, "context:WEB-INF/app.properties");
     }
 
